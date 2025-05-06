9046e3762521cb3d07b7b4cfdad43c2fa7a99326
Bruno Fontenele <brunomatos2505@gmail.com>
2025-05-06 20:25:37 +0100

    Update relu.s

--- relu.s
+++ relu.s
@@ -1,20 +1,27 @@
 .data
+
 # You can change this array to test other values
-array: .word -3, 2, -1, 7, -2   # Initial array values				 
+
+array: .word -3, 2, -1, 7, -2 # Initial array values
 
 .text
 
 main:
-  la a0, array      # a0 = pointer to array
-  li a1, 5          # a1 = number of elements in the array
 
-  jal ra, relu      # Call relu function
+la a0, array # a0 = pointer to array
+li a1, 5 # a1 = number of elements in the array
 
-  # Result: the array now has its negative values replaced by zero
+jal ra, relu # Call relu function
+
+
+# Result: the array now has its negative values replaced by zero
+
 
 exit:
-  li a7, 10              # Exit syscall code
-  ecall                  # Terminate the program
+
+li a7, 10 # Exit syscall code
+
+ecall # Terminate the program
 
 
 # ============================================================
@@ -28,22 +35,25 @@ exit:
 #     this function terminates the program with error code 36
 # ============================================================
 relu:
-    # Verificar se o tamanho do vetor é válido
-    add x28, x0, x0            # load immediate o valor 1 em x6
-    blt a1, x6, exit_with_error  # Se a1 < 1,erro
 
-# TO Do
-start:  
-    lw x7, 0(a0)
-    beq a1, x6, loop_end
+# Verificar se o tamanho do vetor é válido
+
+add x6, x0, x0 # load immediate o valor 1 em t0
+blt a1, x6, exit_with_error # Se a1 < 1,erro
+
+# TO DO
+
+start:
+lw x7, 0(a0)
+
     bgt x7, x0, end
     sw zero, 0(a0)
     
 end:
     addi x6, x6, 1
+    beq a1, x6, loop_end
     addi a0, a0, 4
-    j start
-    
+    j start    
     
 loop_end:
   jr ra                  # normal return
@@ -54,6 +64,9 @@ loop_end:
 # a0 (int) is the error code 
 # You need to load a0 the error to a0 before to jump here
 exit_with_error:
+  li a0, 36
+  li a7, 93            # Exit system call
+  ecall                # Terminate program
   li a0, 36
   li a7, 93            # Exit system call
   ecall                # Terminate program
