cd034b77921efe0cf7f9e0506b377531b0333bdf
bia-mbfa <beatriz.mbfa@gmail.com>
2025-05-29 02:00:30 +0100

    Update readfile.s

--- Projeto 2 IAC - Grupo 11/readfile.s
+++ Projeto 2 IAC - Grupo 11/readfile.s
@@ -5,11 +5,11 @@ blt a2, t0, exit_with_error_42    # Número de bytes passados menor que 1
 mv t1, a1    # Guarda buffer pointer
 mv t2, a2    # Guarda lenght
 
-li a1 0
-li a7 1024    #open (muda a0 para file descriptor)
+li a1 0       # Flag read only
+li a7 1024    # Open (muda a0 para file descriptor)
 ecall
 
-blt a0, zero, exit_with_error_41    # Open devolve erro
+blt a0, zero, exit_with_error_41    # Open devolve erro se file descriptor < 0
 mv t0, a0    # Guarda file descriptor
 mv a1, t1    # Repor ponteiro buffer
 mv a2, t2    # Repor lenght
@@ -17,10 +17,10 @@ mv a2, t2    # Repor lenght
 li a7, 63    # Read ficheiro (muda a0 para numero de bytes)
 ecall
 
-blt a0, zero, exit_with_error_41    # Read devolve erro
+blt a0, zero, exit_with_error_41    # Read devolve erro se byte lidos < 0
 
-mv t3, a0    # Salva valor de retorno
-mv a0, t0    # Move file descriptor para a0
+mv t3, a0    # Salva número de bytes lidos
+mv a0, t0    # Move file descriptor de novo para a0
 
 li a7, 57    # Close ficheiro
 ecall
