3acf057c313c0ceaf8d3e1ee27f6917800da1994
Bruno Fontenele <brunomatos2505@gmail.com>
2025-05-29 13:11:13 +0100

    Update classify.s

--- Projeto 2 IAC - Grupo 11/classify.s
+++ Projeto 2 IAC - Grupo 11/classify.s
@@ -1,1 +1,256 @@
+# ===========================================================
+# Identificacao do grupo:  [T?? para Tagus ou A?? para Alameda]
+#
+# Membros [istID, primeiro + ultimo nome]
+# 1.
+# 2. 
+# 3. 
+#
+# ===========================================================
+# Requisitos do enunciado que *nao* estao corretamente implementados:
+# (indicar um por linha, ou responder "nenhum")
+# -
+#
+# ===========================================================
+# Top-5 das otimizacoes que a vossa solucao incorpora:
+# (maximo 140 caracteres por cada otimizacao)
+#
+# 1.
+#
+# 2.
+#
+# 3.
+#
+# 4.
+#
+# 5.
+#
+# ===========================================================
+
+.data
+
+# ===========================================================
+#Main data structures. These definitions cannot be changed.
+
+h_m0: .word 128
+w_m0: .word 784
+m0: .zero 401408                #h_m0 * w_m0 * 4 bytes
+
+h_m1: .word 10
+w_m1: .word 128
+m1: .zero 5120                  #h_m1 * w_m1 * 4 bytes
+
+h_input: .word 784
+w_input: .word 1
+input: .zero 3136               #h_input * w_input * 4 bytes
+
+h_h: .word 128
+w_h: .word 1
+h: .zero 512                    #h_h * w_h * 4 bytes
+
+h_o: .word 10
+w_o: .word 1
+o: .zero 40                     #h_o * w_o * 4 bytes
+
+
+# ===========================================================
+# Here you can define any additional data structures that your program might need
+
+
+
+
+# ===========================================================
+.text
+
+main:
+    # Set up arguments for *classify* function
+    #
+    # TODO
+    #
+
+    # Call *classify* function
+    # TODO
+    #
+
+    #
+    j exit
+    
+
+# ===========================================================
+# FUNCTION: abs
+#   Computes absolute value of the int stored at a0
+# Arguments:
+#   a0, a pointer to int
+# Returns:
+#   Nothing (modifies value in memory)
+# ===========================================================
+
+    #
+    # TO DO: copy from your previous solution in another file
+    #
+    
+    jr ra                    # Return to the caller
+
+
+
+# ============================================================
+# FUNCTION: relu
+#   Applies ReLU on each element of the array (in-place)
+# Arguments:
+#   a0 = pointer to int array
+#   a1 = array length
+# Exceptions:
+#   - If the length of the array is less than 1,
+#     this function terminates the program with error code 36
+# ============================================================
+relu:
+
+    #
+    # TO DO: copy from your previous solution in another file
+    #
+    
+    jr ra                    # Return to the caller
+
+
+
+# =================================================================
+# FUNCTION: Given an int array, return the index of the largest
+#   element. If there are multiple, return the one
+#   with the smallest index.
+# Arguments:
+#   a0 (int*) is the pointer to the start of the array
+#   a1 (int)  is the number of elements in the array
+# Returns:
+#   a0 (int)  is the first index of the largest element
+# Exceptions:
+#   - If the length of the array is less than 1,
+#     this function terminates the program with error code 37
+# =================================================================
+argmax:
+
+    #
+    # TO DO: copy from your previous solution in another file
+    #
+    
+    jr ra                    # Return to the caller
+
+
+
+
+# =======================================================
+# FUNCTION: Dot product of 2 int arrays
+# Arguments:
+#   a0 (int*) - Pointer to the start of arr0
+#   a1 (int*) - Pointer to the start of arr1
+#   a2 (int)  - Number of elements to use   
+# Returns:
+#   a0 (int)  - The dot product of arr0 and arr1
+# Exceptions:
+#   - If a2 < 1, exit with error code 38
+# =======================================================
+dotproduct:
+
+    #
+    # TO DO: copy from your previous solution in another file
+    #
+    
+    jr ra                    # Return to the caller
+
+
+# =======================================================
+# FUNCTION: Matrix Multiplication of 2 integer matrices
+#   d = matmul(m0, m1)
+#
+# Arguments:
+#   a0 (int*)  - pointer to the start of m0     (Matrix A)
+#   a1 (int*)  - pointer to the start of m1     (Matrix B)
+#   a2 (int)   - number of rows in m0 (A)             [rows_A]
+#   a3 (int)   - number of columns in m0 (A)          [cols_A]
+#   a4 (int)   - number of rows in m1 (B)             [rows_B]
+#   a5 (int)   - number of columns in m1 (B)          [cols_B]
+#   a6 (int*)  - pointer to the start of d            (Matrix C = A x B)
+#
+# Returns:
+#   None (void); result is stored in memory pointed to by a6 (d)
+#
+# Exceptions:
+#  - If the height or width of any of the matrices is less than 1, 
+#    this function terminates the program with error core 39
+#  - If the number of columns in matrix A is not equal to the number 
+#    of rows in matrix B, it terminates with error code 40
+# =======================================================
+matmul:
+
+    #
+    # TO DO: copy from your previous solution in another file
+    #
+    
+    jr ra                    # Return to the caller
+
+
+######################################################################
+# Function: read_file(char* filename, byte* buffer, int length)
+# Input:
+#   a0: pointer to null-terminated filename string
+#   a1: destination buffer
+#   a2: number of bytes to read
+# Output:
+#   a0: number of bytes read (return value from syscall)
+# Exceptions:
+#   - Error code 41 if error in the file descriptor
+#   - Error code 42 If the length of the bytes to read is less than 1
+######################################################################
+
+read_file:
+    
+    #
+    # TO DO: copy from your previous solution in another file
+    #
+    
+    jr ra                    # Return to the caller
+
+
+
+
+# =======================================================
+# FUNCTION: Classify decimal digit from input image
+#   d = classify(A, B, input)
+#
+# Arguments:
+#   a0 (string*)  - pathname of file with the weight matrix m0
+#   a1 (string*)  - pathname of file with the weight matrix m1
+#   a2 (string*)  - pathname of file with the input image in Raw PGM format
+#
+# Returns:
+#   a0 (int) - value of the classified decimal digit
+#
+# =======================================================
+
+classify:
+
+    #
+    # TO DO
+    #
+    
+    jr ra                    # Return to the caller
+
+
+
+
+# =======================================================
+# Exit procedures
+# =======================================================
+
+# Exits the program (with code 0)
+exit:
+    li a7, 10     # Exit syscall code
+    ecall         # Terminate the program
+
+# Exits the program with an error 
+# Arguments: 
+# a0 (int) is the error code 
+# You need to load a0 the error to a0 before to jump here
+exit_with_error:
+  li a7, 93            # Exit system call
+  ecall                # Terminate program
 
