dc8af6926248f356d7e4deeff88e127f1bf00e1e
BiaBlonda <beatrizmedvedyuk@gmail.com>
2025-05-29 01:38:08 +0100

    Update readfile.s

--- Projeto 2 IAC - Grupo 11/readfile.s
+++ Projeto 2 IAC - Grupo 11/readfile.s
@@ -1,1 +1,40 @@
+read_file:
+li t0, 1
+blt a2, t0, exit_with_error_42    # Número de bytes passados menor que 1
 
+mv t1, a1    # Guarda buffer pointer
+mv t2, a2    # Guarda lenght
+
+li a1 0
+li a7 1024    #open (muda a0 para file descriptor)
+ecall
+
+blt a0, zero, exit_with_error_41    # Open devolve erro
+mv t0, a0    # Guarda file descriptor
+mv a1, t1    # Repor ponteiro buffer
+mv a2, t2    # Repor lenght
+
+li a7, 63    # Read ficheiro (muda a0 para numero de bytes)
+ecall
+
+blt a0, zero, exit_with_error_41    # Read devolve erro
+
+mv t3, a0    # Salva valor de retorno
+mv a0, t0    # Move file descriptor para a0
+
+li a7, 57    # Close ficheiro
+ecall
+
+mv a0, t3    # Coloca número de bytes lidos em a0
+jr ra
+
+
+exit_with_error_42:
+    li a0, 42                    # Erro 42
+    li a7, 93                    # Exit system call
+    ecall                        # Terminate programm
+
+exit_with_error_41:
+    li a0, 41                    # Erro 41
+    li a7, 93                    # Exit system call
+    ecall                        # Terminate programm
